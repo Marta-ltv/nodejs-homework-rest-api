ba996d2d9aee52d4f9f7c520d2382ad1412df0f8
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-05-07 12:25:01 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,14 +35,9 @@ const login = async (req, res) => {
     if (!passwordCompare) {
        throw HttpError(401, "Email or password is wrong");
     }
-
-    const payload = {
-        id: user._id,
-    }
-    
+    const payload = {id: user._id,}
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
     await User.findByIdAndUpdate(user._id, { token });
-
     res.json({
         token,
     })
@@ -50,7 +45,6 @@ const login = async (req, res) => {
 
 const getCurrent = async (req, res) => {
     const { email, name } = req.user;
-
     res.json({
         email,
         name,
@@ -60,9 +54,8 @@ const getCurrent = async (req, res) => {
 const logout = async (req, res) => {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, {token: ""});
-
     res.json({
-        message: "Logout success"
+        message: "Not authorized"
     })
 }
 
