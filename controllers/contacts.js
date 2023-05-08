e82013d5cfeb7b3dd501448fc05e392ed932e83d
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-05-08 20:26:07 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,7 @@
 const { HttpError, ctrlWrapper } = require('../helpers');
 const Contact  = require("../models/contact")
 
-const getAll = async (req, res, next) => {
+const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
@@ -9,7 +9,7 @@ const getAll = async (req, res, next) => {
   res.json(result);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
   const { _id } = req.user;
   const { contactId } = req.params;
   const contact = await Contact.findOne({ _id: contactId, owner: _id });
