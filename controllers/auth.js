d67d9041fb371db16e5eefde4eacec050616c18f
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-05-14 18:35:53 +0300

    add changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,57 +1,59 @@
-const {User} = require("../models/user");
-const { HttpError, ctrlWrapper } = require('../helpers');
-const bcrypt = require('bcryptjs');
+const { User } = require("../models/user");
+const { HttpError, ctrlWrapper } = require("../helpers");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
-const { jimp } = require('../middlewares');
+const { jimp } = require("../middlewares");
 
 const { SECRET_KEY } = process.env;
 
-
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (user) {
-        throw HttpError(409, "Email in use");
-    }
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const avatarUrl = gravatar.url(email, { s: '200', r: 'pg', d: '404' });
-    const avatarURL = gravatar.url(email);
+  const avatarUrl = gravatar.url(email, { s: "200", r: "pg", d: "404" });
 
-    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL: avatarUrl,
+  });
 
-    res.status(201).json({
+  res.status(201).json({
     user: {
-        email: newUser.email,
-        subscription: "starter",
-        avatarURL: avatarUrl,
+      email: newUser.email,
+      subscription: "starter",
+      avatarURL: avatarUrl,
     },
   });
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-        
-    if (!user) {
-        throw HttpError(401, "Email or password is wrong");
-    }
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if (!passwordCompare) {
-       throw HttpError(401, "Email or password is wrong");
-    }
-    const payload = {id: user._id,}
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-    await User.findByIdAndUpdate(user._id, { token });
-    res.status(200).json({
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const payload = { id: user._id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.status(200).json({
     token,
     user: {
       email,
-      subscription: 'starter',
+      subscription: "starter",
     },
   });
 };
@@ -66,22 +68,22 @@ const getCurrent = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-    const { _id } = req.user;
-    await User.findByIdAndUpdate(_id, {token: ""});
-    res.status(204).json();
-}
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json();
+};
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
 
   const avatarFileName = `${_id}_${originalname}`;
-  const resultUpload = path.join('public', 'avatars', avatarFileName);
+  const resultUpload = path.join("public", "avatars", avatarFileName);
 
   try {
     await fs.rename(tempUpload, resultUpload);
 
-    const avatarURL = path.join('public', 'avatars', avatarFileName);
+    const avatarURL = path.join("public", "avatars", avatarFileName);
     await User.findByIdAndUpdate(_id, { avatarURL });
 
     res.status(200).json({ avatarURL });
@@ -91,15 +93,10 @@ const updateAvatar = async (req, res) => {
   jimp(resultUpload);
 };
 
-
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-    updateAvatar: ctrlWrapper(updateAvatar),
-}
-
-
-
-
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
+};
