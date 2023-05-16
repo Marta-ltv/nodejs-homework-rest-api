552815d794f10b92b243d578cce22a93ef854f62
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2023-05-16 22:03:30 +0300

    fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,11 +14,9 @@ const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-
   if (user) {
     throw HttpError(409, "Email in use");
   }
-
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarUrl = gravatar.url(email);
 
